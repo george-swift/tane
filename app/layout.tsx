35fa2a9ca0a7fa03f5f5d9105821b1504a4ced1a
George <71434803+george-swift@users.noreply.github.com>
2025-08-29 12:38:48 +0100

    chore(app): update root layout/page and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,56 @@
-// app/layout.tsx
-import "./globals.css";
-import Header from "./Header";
+import type { Metadata } from 'next'
+import { Inter, Poppins } from 'next/font/google'
+import { cn } from '@/lib/utils/classnames'
+import { siteMetadata } from '@/lib/utils/site-metadata'
 
-export const metadata = {
-  title: "Superblog",
-  description: "A blog app using Next.js and Prisma",
-};
+import PrelineScriptWrapper from '@/components/preline-script-wrapper'
+
+import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter'
+})
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['500', '600', '700', '800', '900'],
+  variable: '--font-poppins'
+})
+
+export const metadata: Metadata = {
+  title: siteMetadata.title,
+  description: siteMetadata.description,
+  keywords: [
+    'Next.js',
+    'React',
+    'Tailwind CSS',
+    'Server Components',
+    'Preline UI',
+    'PostgreSQL',
+    'TypeScript',
+    'Prisma',
+    'Better Auth'
+  ]
+}
 
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="h-full">
-      <body className="h-full overflow-hidden">
-        <div className="min-h-screen flex flex-col">
-          <Header />
-          <main className="flex-1 overflow-auto">{children}</main>
-        </div>
+    <html lang="en">
+      <body
+        className={cn(
+          'font-inter h-full antialiased',
+          inter.variable,
+          poppins.variable
+        )}
+      >
+        {children}
       </body>
+      <PrelineScriptWrapper />
     </html>
-  );
+  )
 }
